598d7e302790c950587152bb8fb6dba9beb6305c
clave963 <144608142+clave963@users.noreply.github.com>
2024-02-28 12:40:53 -0500

    Updates to Week5

--- 04_microcontroller/sketch_feb21a/sketch_feb21a.ino
+++ 04_microcontroller/sketch_feb21a/sketch_feb21a.ino
@@ -1,20 +1,24 @@
-/*  Code to run motor in one direction based on potentiometer reading.
- *  Motor driver speed control on pin 3 (direction LOW by default)
- *  Potentiometer plugged into A0, A2, and A4 on Arduino Uno (or similar).
- */
+int ledButton = 3;
+int motorButton = 4;
+int ledPin = 5;
 
-void setup() {
-  pinMode(3, OUTPUT);
-  pinMode(A0, OUTPUT);  //This will be GND for the potentiometer
-  pinMode(A4, OUTPUT);  //This will be 3.3V for the pot
+void setup(){
+  Serial.begin (9600);
+  pinMode(ledButton, INPUT_PULLUP);
+  pinMode (motorButton, INPUT_PULLUP);
 
-  digitalWrite(A0, LOW);
-  digitalWrite(A4, HIGH);
 }
 
 void loop() {
-  int pot_value = analogRead(A2);   //pot wiper is on A2
-  int motor_speed = map(pot_value, 0, 1023, 255, 0); // map motor_level to pot_value so that zero corresponds to lowest speed
-  analogWrite(3, motor_speed);
-  delay(1);
+  Serial.print("Led Button: ");
+  Serial.print(digitalRead(ledButton));
+  Serial.print ("Motor Button: ");
+  Serial.println(digitalRead(motorButton));
+
+  if(digitalRead(ledButton) == 0){
+    digitalWrite(ledPin, HIGH);
+    delay(5000);
+    digitalWrite(ledPin, LOW);
+  }
+
 }
